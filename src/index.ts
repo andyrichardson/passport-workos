879f43ad895d75ff95c3e2918b5623efdfefa595
Andy Richardson <andy.john.richardson@gmail.com>
2021-10-03 15:39:57 +0100

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,83 @@
+import { Strategy } from "passport";
+import { Request } from "express";
+import WorkOS from "@workos-inc/node";
+
+type Options = {
+  clientID: string;
+  clientSecret: string;
+  callbackURL: string;
+  redirectURI: string;
+};
+type Verify = any;
+type AuthenticateOptions = Partial<
+  Parameters<WorkOS["sso"]["getAuthorizationURL"]>[0]
+>;
+
+export class WorkOSSSOStrategy extends Strategy {
+  private client: WorkOS;
+  private options: Options;
+  private verify: Verify;
+
+  constructor(opts: Options, verify: Verify) {
+    super();
+    this.options = opts;
+    this.verify = verify;
+    this.client = new WorkOS(opts.clientSecret);
+  }
+
+  public authenticate(req: Request, options: AuthenticateOptions) {
+    if (req.query?.code) {
+      return this._loginCallback(req, options);
+    }
+
+    return this._loginAttempt(req, options);
+  }
+
+  private _loginAttempt(req: Request, options: AuthenticateOptions) {
+    try {
+      const { connection, domain, email } = req.query as Record<string, string>;
+      if ([connection, domain, email].every((a) => a === undefined)) {
+        throw Error(
+          "One of 'connection', 'domain' and/or 'email' are required"
+        );
+      }
+
+      const url = this.client.sso.getAuthorizationURL({
+        ...req.body,
+        connection,
+        domain: domain || email?.slice(email.indexOf("@") + 1),
+        clientID: this.options.clientID,
+        redirectURI: this.options.callbackURL || options.redirectURI,
+        ...options,
+      });
+      this.redirect(url);
+    } catch (err: any) {
+      this.fail(err);
+    }
+  }
+
+  private async _loginCallback(req: Request, _options: AuthenticateOptions) {
+    const { profile, access_token } = await this.client.sso.getProfileAndToken({
+      code: req.query.code as string,
+      clientID: this.options.clientID,
+    });
+
+    this.verify(
+      req,
+      access_token,
+      undefined /* no refresh token */,
+      profile,
+      (err: any, user: any, info: any) => {
+        if (err) {
+          return this.error(err);
+        }
+
+        if (!user) {
+          return this.fail("no user");
+        }
+
+        return this.success(user, info);
+      }
+    );
+  }
+}
